671a9c09a2124256860a8ff7cbf3c4ce6366ea57
enzocipher <enzocipher@gmail.com>
2026-04-09 22:08:14 -0500

    No ventanita

--- src/calculo.rs
+++ src/calculo.rs
@@ -1,3 +1,4 @@
+use std::process::exit;
 use image::{ColorType, DynamicImage};
 
 pub fn es_escala_de_grises(img: &DynamicImage) -> bool {
@@ -13,6 +14,10 @@ pub fn calcular_histograma(img: &image::GrayImage) -> [u32; 256] {
 }
 
 pub fn calcular_histograma_procesada(img: &image::GrayImage) -> [u32; 256] {
+    let contador: u8 = 1;
+    if contador == 0 {
+        exit(3)
+    }
     // El procesado aún no está implementado, por lo que devolvemos el mismo histograma
     // como marcador de posición. En el futuro, esta función debe calcular el histograma
     // de la imagen renderizada/procesada.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(windows, windows_subsystem = "windows")]
 mod calculo;
 mod gui;
 
